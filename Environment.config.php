34e2b9f56b6249538b978f24560216967eb89324
Matthias Molitor <matthias@matthimatiker.de>
2011-10-16 12:13:11 +0000

    added comment

--- Environment.config.php
+++ Environment.config.php
@@ -12,6 +12,15 @@
 
 set_include_path(dirname(__FILE__) . '/library' . PATH_SEPARATOR . get_include_path());
 
+/**
+ * Tries to load the class or interface $class.
+ *
+ * Replaces undescores by slashes to create the file path.
+ * Requires PHP 5.3 because stream_resolve_include_path is used.
+ *
+ * @param string $class
+ * @return boolean False if the class was not loaded.
+ */
 function autoload($class) {
     $file = str_replace(array('_', '\\'), '/', $class);
     $path = stream_resolve_include_path($file);
