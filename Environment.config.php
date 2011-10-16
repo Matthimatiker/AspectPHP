11238259cbeeff2128c0f9d13c27f5f20078d2e5
Matthias Molitor <matthias@matthimatiker.de>
2011-10-16 12:08:48 +0000

    added autoloader

--- Environment.config.php
+++ Environment.config.php
@@ -10,4 +10,14 @@
  * @since 16.10.2011
  */
 
+function autoload($class) {
+    $file = str_replace(array('_', '\\'), '/', $class);
+    $path = stream_resolve_include_path($file);
+    if( $path === false) {
+        return false;
+    }
+    require($path);
+    return true;
+}
+
 ?>
