e8336e968d0221407032ecdc225b61367ec18962
Matthias Molitor <matthias@matthimatiker.de>
2011-10-16 11:22:09 +0000

    added configuration stub

---
+++ Environment.config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Configures the environment.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectP
+ * @copyright Matthias Molitor 2011
+ * @version $Rev$
+ * @since 16.10.2011
+ */
+
+?>
