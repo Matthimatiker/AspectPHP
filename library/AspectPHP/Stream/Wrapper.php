352a829259d331a132bc4222abb3edc698ca8c14
Matthias Molitor <matthias@matthimatiker.de>
2011-12-13 21:36:58 +0000

    added class stub for a stream wrapper

---
+++ library/AspectPHP/Stream/Wrapper.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * AspectPHP_Stream_Wrapper
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Stream
+ * @copyright Matthias Molitor 2011
+ * @version $Rev$
+ * @since 13.12.2011
+ */
+
+/**
+ * Stream wrapper that adds injection points to loaded php classes.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Stream
+ * @copyright Matthias Molitor 2011
+ * @version $Rev$
+ * @since 13.12.2011
+ */
+class AspectPHP_Stream_Wrapper {
+    
+}
+
+?>
