5e7aad093621ae0aaa23e502a479c85a4dab2587
Matthias Molitor <matthias@matthimatiker.de>
2011-12-13 21:43:02 +0000

    added test case stub for stream wrapper

---
+++ tests/library/AspectPHP/Stream/WrapperTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * AspectPHP_Stream_WrapperTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Stream
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2011
+ * @version $Rev$
+ * @since 13.12.2011
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the AspectPHP stream wrapper.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Stream
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2011
+ * @version $Rev$
+ * @since 13.12.2011
+ */
+class AspectPHP_Stream_WrapperTest extends PHPUnit_Framework_TestCase {
+    
+    /**
+     * Dummy test method.
+     */
+    public function testDummy() {
+        
+    }
+    
+}
+
+?>
---
+++ tests/library/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Initializes the test environment.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package Tests
+ * @copyright Matthias Molitor 2011
+ * @version $Rev$
+ * @since 13.12.2011
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/../bootstrap.php');
+
+?>
