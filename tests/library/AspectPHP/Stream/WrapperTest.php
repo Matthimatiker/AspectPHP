ffca2be0ee19404ad917d7043d6122cfa3c59ccc
Matthias Molitor <matthias@matthimatiker.de>
2011-12-13 22:24:54 +0000

    added comments

--- tests/library/AspectPHP/Stream/WrapperTest.php
+++ tests/library/AspectPHP/Stream/WrapperTest.php
@@ -28,50 +28,92 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
  */
 class AspectPHP_Stream_WrapperTest extends PHPUnit_Framework_TestCase {
     
+    /**
+     * Ensures that is_file() returns true if the given path points
+     * to a existing file.
+     */
     public function testIsFileReturnsTrueIfFileExists() {
         
     }
     
+    /**
+     * Ensures that is_file() returns false if the given path points
+     * to a not existing file.
+     */
     public function testIsFileReturnsFalseIfFileDoesNotExist() {
         
     }
     
+    /**
+     * Checks if stat() returns valid data about the given file.
+     */
     public function testStatReturnsCorrectFileData() {
         
     }
     
+    /**
+     * Ensures that is_writable() returns always false.
+     */
     public function testIsWritableReturnsFalse() {
         
     }
     
+    /**
+     * Ensures that the wrapper cannot be used to modify files.
+     */
     public function testWrapperDoesNotAllowModifyingFiles() {
         
     }
     
+    /**
+     * Checks if the wrapper modifies the loaded code.
+     */
     public function testWrapperModifiesLoadedCode() {
         
     }
     
+    /**
+     * Checks if the wrapper generates valid PHP code.
+     */
     public function testWrapperGeneratesValidCode() {
         
     }
     
+    /**
+     * Ensures that the wrapper can be used to load a file via include()
+     * if the full file path is passed.
+     */
     public function testWrapperCanBeUsedToIncludeFileByFullPath() {
         
     }
     
+    /**
+     * Ensures that the wrapper can be used to load files via include()
+     * by using the include path.
+     */
     public function testWrapperCanBeUsedToIncludeFileFromIncludePath() {
         
     }
     
+    /**
+     * Ensures that the wrapper does not change the original method names.
+     */
     public function testWrapperDoesNotChangeOriginalMethodNames() {
         
     }
     
+    /**
+     * Ensures that the wrapper does not modify the visibility of the original
+     * methods.
+     */
     public function testWrapperDoesNotChangeMethodVisibility() {
         
     }
     
+    /**
+     * Ensures that the wrapper does not suppress errors if invalid
+     * code is loaded.
+     */
     public function testWrapperDoesNotSuppressErrors() {
         
     }
