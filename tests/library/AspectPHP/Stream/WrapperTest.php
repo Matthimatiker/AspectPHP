d59f847fc32097554207ecee34331c60865bf00f
Matthias Molitor <matthias@matthimatiker.de>
2011-12-13 22:02:28 +0000

    added test stubs

--- tests/library/AspectPHP/Stream/WrapperTest.php
+++ tests/library/AspectPHP/Stream/WrapperTest.php
@@ -28,10 +28,51 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
  */
 class AspectPHP_Stream_WrapperTest extends PHPUnit_Framework_TestCase {
     
-    /**
-     * Dummy test method.
-     */
-    public function testDummy() {
+    public function testIsFileReturnsTrueIfFileExists() {
+        
+    }
+    
+    public function testIsFileReturnsFalseIfFileDoesNotExist() {
+        
+    }
+    
+    public function testStatReturnsCorrectFileData() {
+        
+    }
+    
+    public function testIsWritableReturnsFalse() {
+        
+    }
+    
+    public function testWrapperDoesNotAllowModifyingFiles() {
+        
+    }
+    
+    public function testWrapperModifiesLoadedCode() {
+        
+    }
+    
+    public function testWrapperGeneratesValidCode() {
+        
+    }
+    
+    public function testWrapperCanBeUsedToIncludeFileByFullPath() {
+        
+    }
+    
+    public function testWrapperCanBeUsedToIncludeFileFromIncludePath() {
+        
+    }
+    
+    public function testWrapperDoesNotChangeOriginalMethodNames() {
+        
+    }
+    
+    public function testWrapperDoesNotChangeMethodVisibility() {
+        
+    }
+    
+    public function testWrapperDoesNotSuppressErrors() {
         
     }
     
