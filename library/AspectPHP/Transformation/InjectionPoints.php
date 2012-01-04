ff28e779f9a214874014826937d25885754232b4
Matthias Molitor <matthias@matthimatiker.de>
2012-01-04 23:11:32 +0000

    added class stub for code transformation

---
+++ library/AspectPHP/Transformation/InjectionPoints.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_Transformation_InjectionPoints
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Transformation
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 05.01.2012
+ */
+
+/**
+ * Transformation class that adds injection points to the given source code.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Transformation
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 05.01.2012
+ */
+class AspectPHP_Transformation_InjectionPoints {
+    
+    /**
+     * Transforms the source code.
+     *
+     * @param string $source
+     * @return string The transformed code.
+     */
+    public function transform($source) {
+        return $source;
+    }
+    
+}
+
+?>
