a8f5eb7d8af0b40da4e61bfeb6738738de654548
Matthias Molitor <matthias@matthimatiker.de>
2012-01-04 23:15:11 +0000

    added test class stub

---
+++ tests/library/AspectPHP/Transformation/InjectionPointsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * AspectPHP_Transformation_InjectionPointsTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Transformation
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 05.01.2012
+ */
+
+/**
+ * Tests the transformation class that adds injection points.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Transformation
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 05.01.2012
+ */
+class AspectPHP_Transformation_InjectionPointsTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
