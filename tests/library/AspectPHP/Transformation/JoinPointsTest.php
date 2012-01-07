0df567a5fe59c2c2f9dac4ba025601a8518aef8e
Matthias Molitor <matthias@matthimatiker.de>
2012-01-07 15:52:07 +0000

    added test initialization

--- tests/library/AspectPHP/Transformation/JoinPointsTest.php
+++ tests/library/AspectPHP/Transformation/JoinPointsTest.php
@@ -12,6 +12,11 @@
  */
 
 /**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
  * Tests the transformation class that adds injection points.
  *
  * @author Matthias Molitor <matthias@matthimatiker.de>
@@ -22,6 +27,44 @@
  * @since 05.01.2012
  */
 class AspectPHP_Transformation_JoinPointsTest extends PHPUnit_Framework_TestCase {
+    
+    /**
+     * System under test.
+     *
+     * @var AspectPHP_Transformation_JoinPoints
+     */
+    protected $transformation = null;
+    
+    protected $original = null;
+    
+    protected $transformed = null;
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp() {
+        parent::setUp();
+        $this->transformation = new AspectPHP_Transformation_JoinPoints();
+        $this->original       = file_get_contents(dirname(__FILE__) . '/TestData/JoinPointsCheck/Transformation.php');
+        $this->transformed    = $this->transformation->transform($this->original);
+        if( !class_exists('JoinPointsCheck_Transformation', false) ) {
+            eval($this->transformed);
+        }
+    }
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown() {
+        $this->transformed    = null;
+        $this->original       = null;
+        $this->transformation = null;
+        parent::tearDown();
+    }
+    
+    public function testTransformReturnsString() {
+        
+    }
     
     // TODO: add tests
     // adds code
