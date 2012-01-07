b903cc074722b90108e2571bf8b95482467fdccb
Matthias Molitor <matthias@matthimatiker.de>
2012-01-07 19:41:56 +0000

    added first lines of code added todo

--- library/AspectPHP/Transformation/JoinPoints.php
+++ library/AspectPHP/Transformation/JoinPoints.php
@@ -22,12 +22,27 @@
 class AspectPHP_Transformation_JoinPoints {
     
     /**
+     * The tokens of the source code that is currently processed.
+     *
+     * @var array(array(integer=>string|integer)|string)
+     */
+    protected $tokens = array();
+    
+    /**
      * Transforms the source code.
      *
      * @param string $source
      * @return string The transformed code.
      */
     public function transform($source) {
+        // TODO: extract token analyzer class
+        $this->tokens = token_get_all($source);
+        foreach( $this->tokens as $token ) {
+            /* @var array(integer=>string|integer)|string */
+            if( $token[0] === T_FUNCTION ) {
+                
+            }
+        }
         return $source;
     }
     
