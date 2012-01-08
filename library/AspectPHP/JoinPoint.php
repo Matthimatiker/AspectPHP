c6e7915b0b0de2a15ffdbe07105f6aec23582905
Matthias Molitor <matthias@matthimatiker.de>
2012-01-08 22:15:14 +0000

    added join point class stub

---
+++ library/AspectPHP/JoinPoint.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * AspectPHP_JoinPoint
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 08.01.2012
+ */
+
+/**
+ * Encapsulates information about a join point.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 08.01.2012
+ */
+class AspectPHP_JoinPoint {
+    
+}
+
+?>
