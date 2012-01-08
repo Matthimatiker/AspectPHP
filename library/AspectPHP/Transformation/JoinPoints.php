8fa6d766f06d4ac6bee6fa31b47aa565aed0e1c9
Matthias Molitor <matthias@matthimatiker.de>
2012-01-08 01:19:16 +0000

    fixed code formatting

--- library/AspectPHP/Transformation/JoinPoints.php
+++ library/AspectPHP/Transformation/JoinPoints.php
@@ -92,11 +92,11 @@ class AspectPHP_Transformation_JoinPoints {
      * @return string The code of the generated injection point method.
      */
     protected function buildInjectionPoint($signature, $callee, $context) {
-        $template = '    %s'                                                            . PHP_EOL
-                  . '    {'                                                             . PHP_EOL
-                  . '        $args = func_get_args();'                                  . PHP_EOL
+        $template = '    %s'                                                         . PHP_EOL
+                  . '    {'                                                          . PHP_EOL
+                  . '        $args = func_get_args();'                               . PHP_EOL
                   . '        return call_user_func_array(array(%s, \'%s\'), $args);' . PHP_EOL
-                  . '    }'                                                             . PHP_EOL;
+                  . '    }'                                                          . PHP_EOL;
         return sprintf($template, $signature, $context, $callee);
     }
     
