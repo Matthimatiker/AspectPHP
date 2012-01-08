fa0ed4e0c762c0f2b09411223476c46efe731968
Matthias Molitor <matthias@matthimatiker.de>
2012-01-08 23:22:22 +0000

    added helper method

--- tests/library/AspectPHP/JoinPointTest.php
+++ tests/library/AspectPHP/JoinPointTest.php
@@ -28,6 +28,18 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
  */
 class AspectPHP_JoinPointTest extends PHPUnit_Framework_TestCase {
     
+    /**
+     * This method and its parameters are used to create a join point for testing.
+     *
+     * @param string $name
+     * @param boolean $register
+     * @return AspectPHP_JoinPoint
+     */
+    protected function createJoinPoint($name, $register = true) {
+        $arguments = func_get_args();
+        return new AspectPHP_JoinPoint(__METHOD__, $arguments);
+    }
+    
 }
 
 ?>
