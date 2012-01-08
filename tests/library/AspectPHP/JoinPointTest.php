db3d8dcf3b556c8c7cc5cccf92cdbd4db0ddb932
Matthias Molitor <matthias@matthimatiker.de>
2012-01-08 22:18:30 +0000

    added test case stub

---
+++ tests/library/AspectPHP/JoinPointTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * AspectPHP_JoinPointTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 08.01.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the JoinPoint implementation.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 08.01.2012
+ */
+class AspectPHP_JoinPointTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
