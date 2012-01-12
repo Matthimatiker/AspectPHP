e7e81f365ebc8dbb41378a6058b9e32d5ce7c2fa
Matthias Molitor <matthias@matthimatiker.de>
2012-01-12 23:02:14 +0000

    added test case stub

---
+++ tests/library/AspectPHP/Manager/StandardTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * AspectPHP_Manager_StandardTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Manager
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 13.01.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the default aspect manager.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Manager
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 13.01.2012
+ */
+class AspectPHP_Manager_StandardTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
