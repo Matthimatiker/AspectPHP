8d9b50bd4d1c6ac5ddd56372af35486d57c128f9
Matthias Molitor <matthias@matthimatiker.de>
2012-01-12 22:17:03 +0000

    added class stub

---
+++ library/AspectPHP/Environment.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * AspectPHP_Environment
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 12.01.2012
+ */
+
+/**
+ * Helper class that is able to configure the AspectPHP environment.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 12.01.2012
+ */
+class AspectPHP_Environment {
+    
+}
+
+?>
