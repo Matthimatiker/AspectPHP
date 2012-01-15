a98cbf0bd66a210d8f80e9b5c30676393b5c7b9d
Matthias Molitor <matthias@matthimatiker.de>
2012-01-15 18:34:26 +0000

    added test case stub

---
+++ tests/library/AspectPHP/ContainerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * AspectPHP_ContainerTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 15.01.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the container class.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 15.01.2012
+ */
+class AspectPHP_ContainerTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
