93cc13b711de6b40ea20624259e33398d8e3679e
Matthias Molitor <matthias@matthimatiker.de>
2012-01-15 18:32:35 +0000

    added class stub

---
+++ library/AspectPHP/Container.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * AspectPHP_Container
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 15.01.2012
+ */
+
+/**
+ * Class that hold the global aspect manager object.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 15.01.2012
+ */
+class AspectPHP_Container {
+    
+}
+
+?>
