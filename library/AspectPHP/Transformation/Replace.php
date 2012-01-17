da931edb30d8269108dfe58e746ba8044a84b487
Matthias Molitor <matthias@matthimatiker.de>
2012-01-17 22:17:16 +0000

    added transformation class stub

---
+++ library/AspectPHP/Transformation/Replace.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * AspectPHP_Transformation_Replace
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Transformation
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 17.01.2012
+ */
+
+/**
+ * Transformation class that replaces specific tokens by provided values.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Transformation
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 17.01.2012
+ */
+class AspectPHP_Transformation_Replace {
+    
+    /**
+     * Creates the transformation class.
+     *
+     * @param array(integer=>string) $map
+     */
+    public function __construct($map) {
+        
+    }
+    
+    /**
+     * Transforms the provided source code.
+     *
+     * @param string $source
+     * @return string
+     */
+    public function transform($source) {
+        
+    }
+    
+}
+
+?>
