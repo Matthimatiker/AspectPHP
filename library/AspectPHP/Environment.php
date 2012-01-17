f1190461d90503213462d815d906dfcecfcef241
Matthias Molitor <matthias@matthimatiker.de>
2012-01-17 17:13:13 +0000

    implemented parts of the helper class that initializes the AspectPHP environment

--- library/AspectPHP/Environment.php
+++ library/AspectPHP/Environment.php
@@ -22,14 +22,58 @@
 class AspectPHP_Environment {
     
     /**
+     * The aspect manager.
+     *
+     * @var AspectPHP_Manager
+     */
+    protected $manager = null;
+    
+    /**
+     * Returns the aspect manager that will be used.
+     *
+     * @return AspectPHP_Manager
+     */
+    public function getManager() {
+        if( $this->manager === null ) {
+            $this->manager = new AspectPHP_Manager_Standard();
+        }
+        return $this->manager;
+    }
+    
+    /**
+     * Modifies the include path and ensures that the classes
+     * are loaded with the AspectPHP stream.
+     */
+    protected function modifyIncludePath() {
+        $paths         = explode(PATH_SEPARATOR, get_include_path());
+        $numberOfPaths = count($paths);
+        for( $i = 0; $i < $numberOfPaths; $i++ ) {
+            $paths[$i] = $this->toStream($paths[$i]);
+        }
+        set_include_path(implode(PATH_SEPARATOR, $paths));
+    }
+    
+    /**
+     * Adds the AspectPHP stream scheme to the given path.
+     *
+     * @param string $path
+     * @return string
+     */
+    protected function toStream($path) {
+        return AspectPHP_Stream::NAME . '://' . $path;
+    }
+    
+    /**
      * Activates AspectPHP.
      *
      * Returns the manager that can be used to register aspects.
      *
      * @return AspectPHP_Manager
      */
-    public function activate() {
-        
+    public function initialize() {
+        AspectPHP_Stream::register();
+        AspectPHP_Container::setManager($this->getManager());
+        $this->modifyIncludePath();
     }
     
 }
