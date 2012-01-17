02d415dd2f5cddbe277a69d15f8ee8fc3e5442fc
Matthias Molitor <matthias@matthimatiker.de>
2012-01-17 22:53:23 +0000

    added basic initialization

--- tests/library/AspectPHP/Transformation/ReplaceTest.php
+++ tests/library/AspectPHP/Transformation/ReplaceTest.php
@@ -23,6 +23,29 @@
  */
 class AspectPHP_Transformation_ReplaceTest extends PHPUnit_Framework_TestCase {
     
+    /**
+     * System under test.
+     *
+     * @var AspectPHP_Transformation_Replace
+     */
+    protected $transformation = null;
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp() {
+        parent::setUp();
+        $this->transformation = new AspectPHP_Transformation_Replace();
+    }
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown() {
+        $this->transformation = null;
+        parent::tearDown();
+    }
+    
 }
 
 ?>
