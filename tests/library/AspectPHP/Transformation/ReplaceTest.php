eca9f66d50bf88bd2452f63a9879041424ff48a3
Matthias Molitor <matthias@matthimatiker.de>
2012-01-17 22:52:14 +0000

    added test case stub

---
+++ tests/library/AspectPHP/Transformation/ReplaceTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * AspectPHP_Transformation_ReplaceTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Transformation
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 17.01.2012
+ */
+
+/**
+ * Tests the replace transformation.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Transformation
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 17.01.2012
+ */
+class AspectPHP_Transformation_ReplaceTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
