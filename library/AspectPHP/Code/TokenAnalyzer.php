2a1dc08b6fafdd71946679a1bd3058ad3f6b6737
Matthias Molitor <matthias@matthimatiker.de>
2012-01-27 19:06:26 +0000

    added stubs for array access methods

--- library/AspectPHP/Code/TokenAnalyzer.php
+++ library/AspectPHP/Code/TokenAnalyzer.php
@@ -19,7 +19,7 @@
  * @version $Rev$
  * @since 27.01.2012
  */
-class AspectPHP_Code_TokenAnalyzer {
+class AspectPHP_Code_TokenAnalyzer implements ArrayAccess {
     
     /**
      * Creates an analyzer.
@@ -27,6 +27,48 @@ class AspectPHP_Code_TokenAnalyzer {
      * @param array(array|string) $tokens The tokenized source code.
      */
     public function __construct(array $tokens) {
+        
+    }
+    
+    /**
+     * Checks if the token with the given offset exists.
+     *
+     * @param integer $offset
+     * @return boolean
+     */
+    public function offsetExists($offset) {
+        
+    }
+    
+    /**
+     * Returns the token with the given offset.
+     *
+     * @param integer $offset
+     * @return array(mixed)|string
+     * @throws InvalidArgumentException If the token with the provided offset does not exist.
+     */
+    public function offsetGet($offset) {
+        
+    }
+    
+    /**
+     * Modifying tokens is not supported, therefore a BadMethodCallException is thrown.
+     *
+     * @param integer $offset
+     * @param array(mixed)|string $value
+     * @throws BadMethodCallException Always throws an exception.
+     */
+    public function offsetSet($offset, $value) {
+        
+    }
+    
+    /**
+     * Deleting tokens is not supported, therefore a BadMethodCallException is thrown.
+     *
+     * @param integer $offset
+     * @throws BadMethodCallException Always throws an exception.
+     */
+    public function offsetUnset($offset) {
         
     }
     
