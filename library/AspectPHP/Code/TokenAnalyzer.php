9ae2fc1dbeb5dbd1eaccb6a9ce87ecb790dfa75a
Matthias Molitor <matthias@matthimatiker.de>
2012-01-27 17:58:42 +0000

    added class stub for token analyzer

---
+++ library/AspectPHP/Code/TokenAnalyzer.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * AspectPHP_Code_TokenAnalyzer
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 27.01.2012
+ */
+
+/**
+ * Analyzes tokenized source code.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 27.01.2012
+ */
+class AspectPHP_Code_TokenAnalyzer {
+    
+    /**
+     * Creates an analyzer.
+     *
+     * @param array(array|string) $tokens The tokenized source code.
+     */
+    public function __construct(array $tokens) {
+        
+    }
+    
+}
+
+?>
