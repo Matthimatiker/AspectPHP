99236e86b812edca8f12780e3c3e4184346d3217
Matthias Molitor <matthias@matthimatiker.de>
2012-01-27 22:03:17 +0000

    added test case stub

---
+++ tests/library/AspectPHP/Code/TokenAnalyzerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * AspectPHP_Code_TokenAnalyzerTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 27.01.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the token analyzer.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 27.01.2012
+ */
+class AspectPHP_Code_TokenAnalyzerTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
