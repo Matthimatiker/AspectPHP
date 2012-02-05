261469e652218cfc5a6713691e8151521a6c474e
Matthias Molitor <matthias@matthimatiker.de>
2012-02-05 13:32:21 +0000

    added class stub for a token editor

--- library/AspectPHP/Code/TokenEditor.php
+++ library/AspectPHP/Code/TokenEditor.php
@@ -21,6 +21,39 @@
  */
 class AspectPHP_Code_TokenEditor extends AspectPHP_Code_TokenAnalyzer {
     
+    /**
+     * Replaces the token at position $index with $newToken.
+     *
+     * @param integer $index
+     * @param string|array(integer|string) $newToken
+     */
+    public function replace($index, $newToken) {
+        
+    }
+    
+    /**
+     * Removes the token at position $index.
+     *
+     * @param integer $index
+     */
+    public function remove($index) {
+        
+    }
+    
+    /**
+     * Commits all pending changes.
+     */
+    public function commit() {
+        
+    }
+    
+    /**
+     * Discards all queued changes.
+     */
+    public function discard() {
+        
+    }
+    
 }
 
 ?>
