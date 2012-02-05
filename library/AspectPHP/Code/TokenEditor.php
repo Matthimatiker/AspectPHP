44da05bad96960cf03c981daefdc1e3de51133e5
Matthias Molitor <matthias@matthimatiker.de>
2012-02-05 13:23:36 +0000

    added class stub

---
+++ library/AspectPHP/Code/TokenEditor.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * AspectPHP_Code_TokenEditor
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 05.02.2012
+ */
+
+/**
+ * The token editor allows the manipulation of tokenized source code.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 05.02.2012
+ */
+class AspectPHP_Code_TokenEditor extends AspectPHP_Code_TokenAnalyzer {
+    
+}
+
+?>
