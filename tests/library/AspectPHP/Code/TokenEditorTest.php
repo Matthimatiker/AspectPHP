b8ee7bca3121f8fd7d3cb95c22bbea0c6817db0f
Matthias Molitor <matthias@matthimatiker.de>
2012-02-05 14:02:54 +0000

    added test case stub

---
+++ tests/library/AspectPHP/Code/TokenEditorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * AspectPHP_Code_TokenEditorTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 05.02.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the token editor.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 05.02.2012
+ */
+class AspectPHP_Code_TokenEditorTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
