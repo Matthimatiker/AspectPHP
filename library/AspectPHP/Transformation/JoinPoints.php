335c67c7a787d35edbc4c496071f237791007aa8
Matthias Molitor <matthias@matthimatiker.de>
2012-02-06 20:54:47 +0000

    used editor to determine number of tokens

--- library/AspectPHP/Transformation/JoinPoints.php
+++ library/AspectPHP/Transformation/JoinPoints.php
@@ -96,7 +96,7 @@ class AspectPHP_Transformation_JoinPoints {
         $injectionPoints[] = $this->getCode('_aspectPHPInternalHandleCall');
         // Inject new methods at the end of the class body.
         $injectedCode = implode(PHP_EOL, $injectionPoints);
-        $source = $this->between(0, $end - 1) . $injectedCode . $this->between($end, count($this->tokens) - 1);
+        $source = $this->between(0, $end - 1) . $injectedCode . $this->between($end, count($this->editor) - 1);
         
         return $source;
     }
