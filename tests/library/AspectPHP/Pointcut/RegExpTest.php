ff077f39ce192d2b241f47e6ee927f1c58c26e8c
Matthias Molitor <matthias@matthimatiker.de>
2012-02-09 18:58:11 +0100

    added test case stub (#12)

---
+++ tests/library/AspectPHP/Pointcut/RegExpTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * AspectPHP_Pointcut_RegExpTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 09.02.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the regular expression pointcut.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 09.02.2012
+ */
+class AspectPHP_Pointcut_RegExpTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
