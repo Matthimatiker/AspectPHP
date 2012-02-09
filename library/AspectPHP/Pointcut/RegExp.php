9da69091761cc1f7fe6e43e654e1dffc19146fa0
Matthias Molitor <matthias@matthimatiker.de>
2012-02-09 16:14:48 +0100

    added stub for regular expression pointcut (#12)

---
+++ library/AspectPHP/Pointcut/RegExp.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_Pointcut_RegExp
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 09.02.2012
+ */
+
+/**
+ * Pointcut that uses regular expressions to match methods.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 09.02.2012
+ */
+class AspectPHP_Pointcut_RegExp implements AspectPHP_Pointcut {
+    
+    /**
+     * @see AspectPHP_Pointcut::matches()
+     *
+     * @param string $method
+     * @return boolean
+     */
+    public function matches($method) {
+        
+    }
+    
+}
+
+?>
