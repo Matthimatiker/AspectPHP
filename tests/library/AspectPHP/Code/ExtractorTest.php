2b5966374d2a510f46ace00b86bef03f429cc8da
Matthias Molitor <matthias@matthimatiker.de>
2012-02-11 21:05:03 +0100

    added test case stub (#14)

---
+++ tests/library/AspectPHP/Code/ExtractorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * AspectPHP_Code_ExtractorTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @subpackage Test
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 11.02.2012
+ */
+
+/**
+ * Tests the code extractor.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @subpackage Test
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 11.02.2012
+ */
+class AspectPHP_Code_ExtractorTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
