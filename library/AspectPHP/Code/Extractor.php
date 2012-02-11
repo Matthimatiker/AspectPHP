bd6a4af8451ddd372405c08b7130d63893d92aa3
Matthias Molitor <matthias@matthimatiker.de>
2012-02-11 20:53:29 +0100

    added class stub (#14)

---
+++ library/AspectPHP/Code/Extractor.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * AspectPHP_Code_Extractor
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 11.02.2012
+ */
+
+/**
+ * Helper class that may be used to extract source code.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Code
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 11.02.2012
+ */
+class AspectPHP_Code_Extractor {
+    
+    /**
+     *
+     *
+     * @param string $method
+     * @return string
+     * @throws InvalidArgumentException
+     */
+    public function getSource($method) {
+        
+    }
+    
+}
+
+?>
