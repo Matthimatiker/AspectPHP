c5a37b4a152a0a2267e82dafbfb4e895742edbfc
Matthias Molitor <matthias@matthimatiker.de>
2012-03-13 15:40:47 +0100

    commented tests (#14)

--- tests/library/AspectPHP/Code/ExtractorTest.php
+++ tests/library/AspectPHP/Code/ExtractorTest.php
@@ -23,42 +23,76 @@
  */
 class AspectPHP_Code_ExtractorTest extends PHPUnit_Framework_TestCase {
     
+    /**
+     * Ensures that an exception is thrown if an invalid method identifier is provided.
+     */
     public function testGetSourceThrowsExceptionIfInvalidMethodIdentifierIsProvided()
     {
         
     }
     
+    /**
+     * Ensures that an exception is thrown if the class does not exist.
+     */
     public function testGetSourceThrowsExceptionIfClassDoesNotExist()
     {
         
     }
     
+    /**
+     * Ensures that an exception is thrown if the method does not exist.
+     */
     public function testGetSourceThrowsExceptionIfMethodDoesNotExist()
     {
         
     }
     
+    /**
+     * Checks if getSource() returns the method body.
+     */
     public function testGetSourceReturnsMethodBody()
     {
         
     }
     
+    /**
+     * Checks if getSource() returns the method signature.
+     */
     public function testGetSourceReturnsMethodSignature()
     {
         
     }
     
+    /**
+     * Checks if getSource() returns the doc block of the method.
+     */
     public function testGetSourceReturnsMethodDocBlock()
     {
         
     }
     
+    /**
+     * Checks if getSource() returns the correct number of source lines.
+     */
     public function testGetSourceReturnsCorrectNumberOfLines()
     {
         
     }
     
+    /**
+     * Checks if getSource() always returns the same result if the parameter
+     * is not changed.
+     */
     public function testGetSourceIsDeterministic()
+    {
+        
+    }
+    
+    /**
+     * Ensures that getSource() works even if the requested method does not
+     * provide a doc block.
+     */
+    public function testGetSourceWorksEvenIfMethodDoesNotProvideDocBlock()
     {
         
     }
