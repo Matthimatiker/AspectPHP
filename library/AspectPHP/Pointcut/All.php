aa082fb36a5c56507e3fbb39f87224f507d138a2
Matthias Molitor <matthias@matthimatiker.de>
2012-03-14 11:51:19 +0100

    added class stub for pointcut that matches all methods

---
+++ library/AspectPHP/Pointcut/All.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * AspectPHP_Pointcut_All
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 14.03.2012
+ */
+
+/**
+ * A pointcut that matches all methods.
+ *
+ * May be used for testing or as null object.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 14.03.2012
+ */
+class AspectPHP_Pointcut_All implements AspectPHP_Pointcut
+{
+    
+	/**
+     * @see AspectPHP_Pointcut::matches()
+     *
+     * @param string $method
+     * @return boolean
+     */
+    public function matches($method) {
+        
+    }
+    
+}
+
+?>
