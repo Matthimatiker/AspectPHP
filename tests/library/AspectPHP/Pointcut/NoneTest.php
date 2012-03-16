188f6cf58234efad415e17d16b0a6e64b3c27ba1
Matthias Molitor <matthias@matthimatiker.de>
2012-03-16 21:19:34 +0100

    added testcase stub

---
+++ tests/library/AspectPHP/Pointcut/NoneTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * AspectPHP_Pointcut_NoneTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 16.03.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ *
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 16.03.2012
+ */
+class AspectPHP_Pointcut_NoneTest extends PHPUnit_Framework_TestCase
+{
+    
+}
+
+?>
