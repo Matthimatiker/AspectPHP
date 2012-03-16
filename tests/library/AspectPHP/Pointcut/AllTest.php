0e219d25851ba6680dab48a6178d520dc0482937
Matthias Molitor <matthias@matthimatiker.de>
2012-03-16 20:53:58 +0100

    added testcase stub

---
+++ tests/library/AspectPHP/Pointcut/AllTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * AspectPHP_Pointcut_AllTest
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 16.03.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the All pointcut.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @subpackage Tests
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 16.03.2012
+ */
+class AspectPHP_Pointcut_AllTest extends PHPUnit_Framework_TestCase
+{
+    
+}
+
+?>
