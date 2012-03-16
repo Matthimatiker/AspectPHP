8036082169ae655dcfca4ff4a7e6457dbe24f5a8
Matthias Molitor <matthias@matthimatiker.de>
2012-03-16 21:20:30 +0100

    added testcase initialization

--- tests/library/AspectPHP/Pointcut/NoneTest.php
+++ tests/library/AspectPHP/Pointcut/NoneTest.php
@@ -17,7 +17,7 @@
 require_once(dirname(__FILE__) . '/bootstrap.php');
 
 /**
- *
+ * Tests the None pointcut.
  *
  * @author Matthias Molitor <matthias@matthimatiker.de>
  * @package AspectPHP_Pointcut
@@ -28,6 +28,29 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
  */
 class AspectPHP_Pointcut_NoneTest extends PHPUnit_Framework_TestCase
 {
+    
+	/**
+     * System under test.
+     *
+     * @var AspectPHP_Pointcut_None
+     */
+    protected $pointcut = null;
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp() {
+        parent::setUp();
+        $this->pointcut = new AspectPHP_Pointcut_None();
+    }
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown() {
+        $this->pointcut = null;
+        parent::tearDown();
+    }
     
 }
 
