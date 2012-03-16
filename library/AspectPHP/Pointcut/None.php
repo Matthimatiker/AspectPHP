fc6a505c4c3585a21fc663af62fbe063742593cd
Matthias Molitor <matthias@matthimatiker.de>
2012-03-16 21:17:06 +0100

    added class stub for None pointcut

---
+++ library/AspectPHP/Pointcut/None.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * AspectPHP_Pointcut_None
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 16.03.2012
+ */
+
+/**
+ * Pointcut that does not match any method.
+ *
+ * May be used for testing or as null object.
+ *
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @package AspectPHP_Pointcut
+ * @copyright Matthias Molitor 2012
+ * @version $Rev$
+ * @since 16.03.2012
+ */
+class AspectPHP_Pointcut_None implements AspectPHP_Pointcut
+{
+    
+	/**
+     * @see AspectPHP_Pointcut::matches()
+     *
+     * @param string $method
+     * @return boolean
+     */
+    public function matches($method) {
+        
+    }
+    
+}
+
+?>
