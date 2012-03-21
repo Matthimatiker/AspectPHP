e58dab0d2392978ac6d0dc9cf11bd02878c641ae
Matthias Molitor <matthias@matthimatiker.de>
2012-03-21 13:07:18 +0100

    added interface stub (#13)

---
+++ library/AspectPHP/Transformation.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * AspectPHP_Transformation
+ *
+ * @category PHP
+ * @package AspectPHP_Transformation
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 21.03.2012
+ */
+
+/**
+ * Common interface for classes that are able to transform source code.
+ *
+ * @category PHP
+ * @package AspectPHP_Transformation
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 21.03.2012
+ */
+interface AspectPHP_Transformation
+{
+    
+}
