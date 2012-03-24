ed621caed76055bc51c6c59e0e8cf423c18d110b
Matthias Molitor <matthias@matthimatiker.de>
2012-03-24 13:44:30 +0100

    added class stub for Not pointcut

---
+++ library/AspectPHP/Pointcut/Not.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * AspectPHP_Pointcut_Not
+ *
+ * @category PHP
+ * @package AspectPHP_Pointcut
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 24.03.2012
+ */
+
+/**
+ * Pointcut that negates the result of its inner pointcut.
+ *
+ * @category PHP
+ * @package AspectPHP_Pointcut
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 24.03.2012
+ */
+class AspectPHP_Pointcut_Not implements AspectPHP_Pointcut
+{
+    
+    /**
+     * See {@link AspectPHP_Pointcut::matches()} for details.
+     *
+     * @param string $method
+     * @return boolean
+     */
+    public function matches($method)
+    {
+        
+    }
+    
+}
