b708cd219a0c615be85b749c8c9063ea7293f789
Matthias Molitor <matthias@matthimatiker.de>
2012-03-24 13:46:50 +0100

    added test case stub

---
+++ tests/library/AspectPHP/Pointcut/NotTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * AspectPHP_Pointcut_NotTest
+ *
+ * @category PHP
+ * @package AspectPHP_Pointcut
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 24.03.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the Not pointcut.
+ *
+ * @category PHP
+ * @package AspectPHP_Pointcut
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 24.03.2012
+ */
+class AspectPHP_Pointcut_NotTest extends PHPUnit_Framework_TestCase
+{
+    
+    
+    
+}
