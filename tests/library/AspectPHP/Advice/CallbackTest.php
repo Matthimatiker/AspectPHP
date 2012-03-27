8dd05232ed2ea943e1af4a33ea231f422635cf7c
Matthias Molitor <matthias@matthimatiker.de>
2012-03-27 23:24:53 +0200

    added test stub

---
+++ tests/library/AspectPHP/Advice/CallbackTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_Advice_Callback
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 27.03.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the callback advice implementation.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 27.03.2012
+ */
+class AspectPHP_Advice_CallbackTest extends PHPUnit_Framework_TestCase
+{
+    
+}
---
+++ tests/library/AspectPHP/Advice/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Initializes the test environment.
+ *
+ * @category PHP
+ * @package Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2011-2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 27.03.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/../bootstrap.php');
