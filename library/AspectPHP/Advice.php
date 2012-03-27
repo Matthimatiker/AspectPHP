c06da82b1c4be74369790472638088b149647fea
Matthias Molitor <matthias@matthimatiker.de>
2012-03-27 18:56:27 +0200

    added advice interface

---
+++ library/AspectPHP/Advice.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * AspectPHP_Advice
+ *
+ * @category PHP
+ * @package AspectPHP
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 27.03.2012
+ */
+
+/**
+ * Interface for advices.
+ *
+ * An advice consists of a pointcut and an advice callback.
+ *
+ * @category PHP
+ * @package AspectPHP
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 27.03.2012
+ */
+interface AspectPHP_Advice
+{
+    
+    /**
+     * Returns the pointcut.
+     *
+     * @return AspectPHP_Pointcut
+     */
+    public function getPointcut();
+    
+    /**
+     * Executes the advice code.
+     *
+     * @param AspectPHP_JoinPoint $joinPoint
+     */
+    public function invoke(AspectPHP_JoinPoint $joinPoint);
+    
+}
