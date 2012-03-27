c353cc6f42a39ec0e3d5bb6544affe0baca8ba86
Matthias Molitor <matthias@matthimatiker.de>
2012-03-27 20:13:09 +0200

    added class stub for an advice that executes a callback

---
+++ library/AspectPHP/Advice/Callback.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * AspectPHP_Advice_Callback
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 27.03.2012
+ */
+
+/**
+ * Encapsulates a pointcut and a callback that is used to execute advice code.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 27.03.2012
+ */
+class AspectPHP_Advice_Callback
+{
+    
+}
