0633a007c8dd9e5c4b487eca9e0fe198845eaf87
Matthias Molitor <matthias@matthimatiker.de>
2012-03-28 23:10:05 +0200

    added class stub

---
+++ library/AspectPHP/Advice/Composite.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * AspectPHP_Advice_Composite
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.03.2012
+ */
+
+/**
+ * Class that encapsulates multiple advices and behaves like a single advice.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.03.2012
+ */
+class AspectPHP_Advice_Composite
+{
+    
+}
