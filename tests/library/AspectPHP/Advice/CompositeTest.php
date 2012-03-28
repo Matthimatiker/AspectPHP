f4ab3d034c86c3603c1122aa42d4cfecfb6c9b68
Matthias Molitor <matthias@matthimatiker.de>
2012-03-28 23:11:26 +0200

    added test case stub

---
+++ tests/library/AspectPHP/Advice/CompositeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_Advice_CompositeTest
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.03.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the composite advice.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.03.2012
+ */
+class AspectPHP_Advice_CompositeTest extends PHPUnit_Framework_TestCase
+{
+    
+}
