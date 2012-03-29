8cf33836792324e167c3c6a26e88b835de9a8875
Matthias Molitor <matthias@matthimatiker.de>
2012-03-29 23:30:23 +0200

    added advice container class stub

---
+++ library/AspectPHP/Advice/Container.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * AspectPHP_Advice_Container
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 29.03.2012
+ */
+
+/**
+ * Container class that holds an arbitrary number of advices grouped by type.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 29.03.2012
+ */
+class AspectPHP_Advice_Container
+{
+    
+}
