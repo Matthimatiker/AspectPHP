d9f7a0f18fea166b810f2499193f86b6d7a3bc11
Matthias Molitor <matthias@matthimatiker.de>
2012-03-29 23:46:29 +0200

    added test case stub

---
+++ tests/library/AspectPHP/Advice/ContainerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_Advice_ContainerTest
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 29.03.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the advice container implementation.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 29.03.2012
+ */
+class AspectPHP_Advice_ContainerTest extends PHPUnit_Framework_TestCase
+{
+    
+}
