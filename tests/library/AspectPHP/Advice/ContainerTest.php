451436826b88969ff982f75e787874152ed598b5
Matthias Molitor <matthias@matthimatiker.de>
2012-03-29 23:50:35 +0200

    added comments

--- tests/library/AspectPHP/Advice/ContainerTest.php
+++ tests/library/AspectPHP/Advice/ContainerTest.php
@@ -33,41 +33,65 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
 class AspectPHP_Advice_ContainerTest extends PHPUnit_Framework_TestCase
 {
     
+    /**
+     * Checks if before() returns an AspectPHP_Advice_Composite object.
+     */
     public function testBeforeReturnsComposite()
     {
         
     }
     
+    /**
+     * Checks if afterReturning() returns an AspectPHP_Advice_Composite object.
+     */
     public function testAfterReturningReturnsComposite()
     {
         
     }
     
+    /**
+     * Checks if afterThrowing() returns an AspectPHP_Advice_Composite object.
+     */
     public function testAfterThrowingReturnsComposite()
     {
         
     }
     
+    /**
+     * Checks if after() returns an AspectPHP_Advice_Composite object.
+     */
     public function testAfterReturnsComposite()
     {
         
     }
     
+    /**
+     * Ensures that each call to before() returns the same object.
+     */
     public function testBeforeReturnsSameObjectForEachCall()
     {
         
     }
     
+    /**
+     * Ensures that each call to afterReturning() returns the same object.
+     */
     public function testAfterReturningReturnsSameObjectForEachCall()
     {
     
     }
     
+    /**
+     * Ensures that each call to afterThrowing() returns the same object.
+     */
     public function testAfterThrowingReturnsSameObjectForEachCall()
     {
     
     }
     
+    /**
+     * Ensures that each call to after() returns the same object.
+     */
     public function testAfterReturnsSameObjectForEachCall()
     {
     
