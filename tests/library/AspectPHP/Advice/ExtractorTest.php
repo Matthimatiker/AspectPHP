619db2eb470e2b13cbb3507b2711d082f40b4113
Matthias Molitor <matthias@matthimatiker.de>
2012-03-31 19:00:13 +0200

    added test case stub

---
+++ tests/library/AspectPHP/Advice/ExtractorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * AspectPHP_Advice_ExtractorTest
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 31.03.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Test the advice extractor.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 31.03.2012
+ */
+class AspectPHP_Advice_ExtractorTest extends PHPUnit_Framework_TestCase
+{
+    
+}
