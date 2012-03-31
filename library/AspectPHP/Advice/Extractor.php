867c233a68b700c850d08088daeb1148954d4188
Matthias Molitor <matthias@matthimatiker.de>
2012-03-31 18:57:00 +0200

    added class stub

---
+++ library/AspectPHP/Advice/Extractor.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * AspectPHP_Advice_Extractor
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 31.03.2012
+ */
+
+/**
+ * Extracts advices from aspects.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 31.03.2012
+ */
+class AspectPHP_Advice_Extractor
+{
+    
+    /**
+     * Extracts the advices from the given aspect.
+     *
+     * @param AspectPHP_Aspect $aspect
+     * @return AspectPHP_Advice_Container A container that contains the advices grouped by type.
+     */
+    public function getAdvicesFrom(AspectPHP_Aspect $aspect)
+    {
+        
+    }
+    
+}
