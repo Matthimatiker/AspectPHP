d7bb19b1ba4342a709446261e187006c53214490
Matthias Molitor <matthias@matthimatiker.de>
2012-04-01 12:39:14 +0200

    added test data class

---
+++ tests/library/AspectPHP/Advice/TestData/Extractor/NoDocCommentAspect.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Extractor_NoDocCommentAspect
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 01.04.2012
+ */
+
+/**
+ * An aspect that contains a method without doc comment.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 01.04.2012
+ */
+class Extractor_NoDocCommentAspect implements AspectPHP_Aspect
+{
+    
+    public function notDocumentedMethod()
+    {
+    }
+    
+    /**
+     * Returns a pointcut.
+     *
+     * @return AspectPHP_Pointcut
+     */
+    public function pointcut()
+    {
+        return new AspectPHP_Pointcut_All();
+    }
+    
+    /**
+     * An advice that is not public.
+     *
+     * @before pointcut()
+     */
+    public function beforeAdvice()
+    {
+    }
+    
+}
