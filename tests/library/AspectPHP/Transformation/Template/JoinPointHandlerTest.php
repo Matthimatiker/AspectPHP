9b753bfe6ac92bd752640378faff2f0a089e632e
Matthias Molitor <matthias@matthimatiker.de>
2012-04-04 21:03:13 +0200

    added test case stub

---
+++ tests/library/AspectPHP/Transformation/Template/JoinPointHandlerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_Transformation_Template_JoinPointHandlerTest
+ *
+ * @category PHP
+ * @package AspectPHP_Transformation
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 04.04.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the JoinPointHandler template.
+ *
+ * @category PHP
+ * @package AspectPHP_Transformation
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 04.04.2012
+ */
+class AspectPHP_Transformation_Template_JoinPointHandlerTest extends PHPUnit_Framework_TestCase
+{
+    
+}
