142e4530383ff960e5edd71c407749231aae7524
Matthias Molitor <matthias@matthimatiker.de>
2012-04-05 22:34:31 +0200

    added test case stub for example script

---
+++ tests/example/ExampleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_ExampleTest
+ *
+ * @category PHP
+ * @package AspectPHP_Example
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 05.04.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the example script.
+ *
+ * @category PHP
+ * @package AspectPHP_Example
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 05.04.2012
+ */
+class AspectPHP_ExampleTest extends PHPUnit_Framework_TestCase
+{
+    
+}
