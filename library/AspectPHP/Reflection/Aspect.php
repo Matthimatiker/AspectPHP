bcadd422f913a69190c2130165df2eacb9591cb3
Matthias Molitor <matthias@matthimatiker.de>
2012-04-09 20:09:00 +0200

    added stub for aspect reflection class

---
+++ library/AspectPHP/Reflection/Aspect.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * AspectPHP_Reflection_Aspect
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 09.04.2012
+ */
+
+/**
+ * Reflection class that is used to gather information about an aspect.
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 09.04.2012
+ */
+class AspectPHP_Reflection_Aspect
+{
+    
+}
