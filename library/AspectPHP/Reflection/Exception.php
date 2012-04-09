db82d44da162aedf79423a8e412e88b52bcb27db
Matthias Molitor <matthias@matthimatiker.de>
2012-04-09 20:10:56 +0200

    added exception class stub

---
+++ library/AspectPHP/Reflection/Exception.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * AspectPHP_Reflection_Exception
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 09.04.2012
+ */
+
+/**
+ * Exception that is used an error occurs during reflection tasks.
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 09.04.2012
+ */
+class AspectPHP_Reflection_Exception
+{
+    
+}
