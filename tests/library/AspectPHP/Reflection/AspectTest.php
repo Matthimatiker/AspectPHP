834965710f2072063b645f355d53412ad1512456
Matthias Molitor <matthias@matthimatiker.de>
2012-04-11 00:25:38 +0200

    added comments

--- tests/library/AspectPHP/Reflection/AspectTest.php
+++ tests/library/AspectPHP/Reflection/AspectTest.php
@@ -33,131 +33,219 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
 class AspectPHP_Reflection_AspectTest extends PHPUnit_Framework_TestCase
 {
     
+    /**
+     * Ensures that the constructor throws an exception if the provided class
+     * is not an aspect.
+     */
     public function testConstructorThrowsExceptionIfProvidedClassIsNotAnAspect()
     {
         
     }
     
+    /**
+     * Ensures that the constructor throws an exception if the provided object
+     * is not an aspect.
+     */
     public function testConstructorThrowsExceptionIfProvidedObjectIsNotAnAspect()
     {
         
     }
     
+    /**
+     * Checks if the constructor accepts aspect classes.
+     */
     public function testConstructorAcceptsAspectClass()
     {
         
     }
     
+    /**
+     * Checks if the constructor accepts aspect objects.
+     */
     public function testConstructorAcceptsAspectObject()
     {
         
     }
     
+    /**
+     * Checks if getPointcuts() returns an array.
+     */
     public function testGetPointcutsReturnsArray()
     {
         
     }
     
+    /**
+     * Ensures that the array from getPointcuts() is numerical indexed.
+     */
     public function testGetPointcutsReturnsNumericalIndexedArray()
     {
         
     }
     
+    /**
+     * Checks if the array from getPointcuts() contains only ReflectionMethod objects.
+     */
     public function testGetPointcutsReturnsReflectionMethodObjects()
     {
         
     }
     
+    /**
+     * Checks if getPointcuts() returns the expected number of methods.
+     */
     public function testGetPointcutsReturnsCorrectNumberOfMethods()
     {
         
     }
     
+    /**
+     * Checks if getAdvices() returns an array.
+     */
     public function testGetAdvicesReturnsArray()
     {
         
     }
     
+    /**
+     * Ensures that the array from getAdvices() is numerical indexed.
+     */
     public function testGetAdvicesReturnsNumericalIndexedArray()
     {
     
     }
     
+    /**
+     * Checks if the array from getAdvices() contains only ReflectionMethod objects.
+     */
     public function testGetAdvicesReturnsReflectionMethodObjects()
     {
     
     }
     
+    /**
+     * Checks if getAdvices() returns the expected number of methods.
+     */
     public function testGetAdvicesReturnsCorrectNumberOfMethods()
     {
     
     }
     
+    /**
+     * Ensures that getPointcut() throws an exception if the method with the provided
+     * name does not exist.
+     */
     public function testGetPointcutThrowsExceptionIfNameOfNotExistingMethodIsProvided()
     {
         
     }
     
+    /**
+     * Ensures that getPointcut() throws an exception if the provided method is not
+     * considered as pointcut.
+     */
     public function testGetPointcutThrowsExceptionIfMethodIsNotConsideredAsPointcut()
     {
         
     }
     
+    /**
+     * Checks if getPointcut() returns a ReflectionMethod object.
+     */
     public function testGetPointcutReturnsReflectionMethodObject()
     {
         
     }
     
+    /**
+     * Checks if getPointcut() returns the correct method.
+     */
     public function testGetPointcutReturnsCorrectReflectionMethodObject()
     {
     
     }
     
+    /**
+     * Ensures that getAdvice() throws an exception if the method with the provided
+     * name does not exist.
+     */
     public function testGetAdviceThrowsExceptionIfNameOfNotExistingMethodIsProvided()
     {
     
     }
     
+    /**
+     * Ensures that getAdvice() throws an exception if the provided method is not
+     * considered as advice.
+     */
     public function testGetAdviceThrowsExceptionIfMethodIsNotConsideredAsAdvice()
     {
     
     }
     
+    /**
+     * Checks if getAdvice() returns a ReflectionMethod object.
+     */
     public function testGetAdviceReturnsReflectionMethodObject()
     {
     
     }
     
+    /**
+     * Checks if getAdvice() returns the correct method.
+     */
     public function testGetAdviceReturnsCorrectReflectionMethodObject()
     {
     
     }
     
+    /**
+     * Ensures that hasPointcut() returns false if the name of a not existing
+     * method is provided.
+     */
     public function testHasPointcutReturnsFalseIfNotExistingMethodIsProvided()
     {
         
     }
     
+    /**
+     * Ensures that hasPointcut() returns false if the provided method is not
+     * considered as pointcut.
+     */
     public function testHasPointcutReturnsFalseIfMethodIsNotConsideredAsPointcut()
     {
         
     }
     
+    /**
+     * Ensures that hasPointcut() returns true if the pointcut method exists.
+     */
     public function testHasPointcutReturnsTrueIfPointcutExists()
     {
         
     }
     
+    /**
+     * Ensures that hasAdvice() returns false if the name of a not existing
+     * method is provided.
+     */
     public function testHasAdviceReturnsFalseIfNotExistingMethodIsProvided()
     {
     
     }
     
+    /**
+     * Ensures that hasAdvice() returns false if the provided method is not
+     * considered as advice.
+     */
     public function testHasAdviceReturnsFalseIfMethodIsNotConsideredAsAdvice()
     {
     
     }
     
+    /**
+     * Ensures that hasAdvice() returns true if the advice method exists.
+     */
     public function testHasAdviceReturnsTrueIfAdviceExists()
     {
     
