d9a3d3578ecb5b146ae4288a69ea9c008de5b1b5
Matthias Molitor <matthias@matthimatiker.de>
2012-04-10 23:58:34 +0200

    added test case stub

---
+++ tests/library/AspectPHP/Reflection/AspectTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_Reflection_AspectTest
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 10.04.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the aspect reflection implementation.
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 10.04.2012
+ */
+class AspectPHP_Reflection_AspectTest extends PHPUnit_Framework_TestCase
+{
+    
+}
