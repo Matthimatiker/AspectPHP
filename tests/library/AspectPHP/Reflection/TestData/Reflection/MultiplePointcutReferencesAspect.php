9e65127ec6bd7da5378b42b55903b9bf329bb111
Matthias Molitor <matthias@matthimatiker.de>
2012-04-13 20:30:57 +0200

    added test data class

---
+++ tests/library/AspectPHP/Reflection/TestData/Reflection/MultiplePointcutReferencesAspect.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Reflection_MultipleReferencedPointcutAspect
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 13.04.2012
+ */
+
+/**
+ * Aspect with a pointcut that is referenced by multiple advices.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 13.04.2012
+ */
+class Reflection_MultiplePointcutReferencesAspect implements AspectPHP_Aspect
+{
+    
+    /**
+     * Returns a pointcut.
+     *
+     * @return AspectPHP_Pointcut
+     */
+    public function pointcut()
+    {
+        return new AspectPHP_Pointcut_All();
+    }
+    
+    /**
+     * A dummy advice.
+     *
+     * @before pointcut()
+     */
+    public function firstAdvice()
+    {
+    }
+    
+    /**
+     * A dummy advice.
+     *
+     * @after pointcut()
+     */
+    public function secondAdvice()
+    {
+    }
+    
+}
