c7d9de0d707a4f923e932c9a7767468099a0d7fd
Matthias Molitor <matthias@matthimatiker.de>
2012-04-14 17:30:43 +0200

    implemented test added required test data

--- tests/library/AspectPHP/Reflection/AspectTest.php
+++ tests/library/AspectPHP/Reflection/AspectTest.php
@@ -335,7 +335,8 @@ class AspectPHP_Reflection_AspectTest extends PHPUnit_Framework_TestCase
      */
     public function testAspectReflectionSkipsMethodsWithoutDocBlock()
     {
-        $this->markTestIncomplete();
+        $this->setExpectedException(null);
+        $this->createReflection('Reflection_NoDocBlockAspect');
     }
     
     /**
---
+++ tests/library/AspectPHP/Reflection/TestData/Reflection/NoDocBlockAspect.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Reflection_NoDocBlockAspect
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 14.04.2012
+ */
+
+/**
+ * An aspect that contains a method without doc block.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 14.04.2012
+ */
+class Reflection_NoDocBlockAspect implements AspectPHP_Aspect
+{
+    
+    public function notDocumentedMethod()
+    {
+    }
+    
+    /**
+     * Returns a pointcut.
+     *
+     * @return AspectPHP_Pointcut
+     */
+    public function pointcut()
+    {
+        return new AspectPHP_Pointcut_All();
+    }
+    
+    /**
+     * A dummy advice.
+     *
+     * @before pointcut()
+     */
+    public function beforeAdvice()
+    {
+    }
+    
+}
