3d061ba3e33ff61ac5ca894476f8b1d9655f982b
Matthias Molitor <matthias@matthimatiker.de>
2012-04-15 17:09:51 +0200

    added ToDo

--- library/AspectPHP/Reflection/Aspect.php
+++ library/AspectPHP/Reflection/Aspect.php
@@ -70,6 +70,7 @@ class AspectPHP_Reflection_Aspect extends ReflectionClass
     {
         parent::__construct($classOrAspect);
         if (!$this->implementsInterface('AspectPHP_Aspect')) {
+            // TODO: add class name to message
             $message = 'Provided class/object is not an aspect.';
             throw new AspectPHP_Reflection_Exception($message);
         }
