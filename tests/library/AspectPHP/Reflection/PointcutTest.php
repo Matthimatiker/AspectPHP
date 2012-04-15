44475c5179785e8aa15d67673f077649d66440af
Matthias Molitor <matthias@matthimatiker.de>
2012-04-15 15:05:42 +0200

    added test case stub

---
+++ tests/library/AspectPHP/Reflection/PointcutTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_Reflection_PointcutTest
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 15.04.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the pointcut reflection object.
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 15.04.2012
+ */
+class AspectPHP_Reflection_PointcutTest extends PHPUnit_Framework_TestCase
+{
+    
+}
