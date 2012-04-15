542681032c6314e67a18388c0a4aab67aabdbe41
Matthias Molitor <matthias@matthimatiker.de>
2012-04-15 19:33:30 +0200

    added advice reflection class stub

---
+++ library/AspectPHP/Reflection/Advice.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * AspectPHP_Reflection_Advice
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 15.04.2012
+ */
+
+/**
+ * Represents an advice method.
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 15.04.2012
+ */
+class AspectPHP_Reflection_Advice extends AspectPHP_Reflection_Method
+{
+    
+}
