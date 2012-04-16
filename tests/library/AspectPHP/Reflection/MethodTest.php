267e0e462032366fb52a43668cee39eea9937038
Matthias Molitor <matthias@matthimatiker.de>
2012-04-16 22:36:55 +0200

    added test case stub

---
+++ tests/library/AspectPHP/Reflection/MethodTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * AspectPHP_Reflection_MethodTest
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 16.04.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the aspect method reflection class.
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 16.04.2012
+ */
+class AspectPHP_Reflection_MethodTest extends PHPUnit_Framework_TestCase
+{
+    
+}
