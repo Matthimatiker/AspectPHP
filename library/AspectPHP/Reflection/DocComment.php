ededef845d6e92a5d96c519c3e57bb0fe9afc52d
Matthias Molitor <matthias@matthimatiker.de>
2012-04-16 22:31:12 +0200

    added doc comment reflection class stub

---
+++ library/AspectPHP/Reflection/DocComment.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * AspectPHP_Reflection_DocComment
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 16.04.2012
+ */
+
+/**
+ * Represents a doc comment.
+ *
+ * Provides method to extract information from the comment.
+ *
+ * @category PHP
+ * @package AspectPHP_Reflection
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 16.04.2012
+ */
+class AspectPHP_Reflection_DocComment
+{
+    
+}
