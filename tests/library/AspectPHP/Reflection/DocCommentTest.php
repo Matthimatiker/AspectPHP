cf3084f9d8568855054d4b05ddfb77f8b7b9f5a5
Matthias Molitor <matthias@matthimatiker.de>
2012-04-17 22:57:36 +0200

    added test initialization

--- tests/library/AspectPHP/Reflection/DocCommentTest.php
+++ tests/library/AspectPHP/Reflection/DocCommentTest.php
@@ -33,4 +33,50 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
 class AspectPHP_Reflection_DocCommentTest extends PHPUnit_Framework_TestCase
 {
     
+    /**
+     * System under test.
+     *
+     * @var AspectPHP_Reflection_DocComment
+     */
+    protected $docComment = null;
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->docComment = new AspectPHP_Reflection_DocComment($this->getComment());
+    }
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->docComment = null;
+        parent::tearDown();
+    }
+    
+    /**
+     * Returns a comment string for testing.
+     *
+     * @return string
+     */
+    protected function getComment()
+    {
+        $comment = '/**'
+                 . ' * This is a comment.'
+                 . ' *'
+                 . ' * This is the long description.'
+                 . ' *'
+                 . ' * @param JoinPoint $joinPoint'
+                 . ' * @param mixed|null $context'
+                 . ' * @return string'
+                 . ' * @afterThrowing myPointcut()'
+                 . ' * @tagged'
+                 . ' */';
+        return $comment;
+    }
+    
 }
