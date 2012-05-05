a89918345223a3d43fdf6110f9a4e16cb1993832
Matthias Molitor <matthias@matthimatiker.de>
2012-05-05 22:09:32 +0200

    added comments

--- library/AspectPHP/Advice/Types.php
+++ library/AspectPHP/Advice/Types.php
@@ -26,24 +26,63 @@
 class AspectPHP_Advice_Types
 {
     
+    /**
+     * Type name for before advices.
+     *
+     * @var string
+     */
     const BEFORE = 'before';
     
+    /**
+     * Type name for after throwing advices.
+     *
+     * @var string
+     */
     const AFTER_THROWING = 'afterThrowing';
     
+    /**
+     * Type name for after returning advices.
+     *
+     * @var string
+     */
     const AFTER_RETURNING = 'afterReturning';
     
+    /**
+     * Type name for after advices.
+     *
+     * @var string
+     */
     const AFTER = 'after';
     
+    /**
+     * Returns all available types.
+     *
+     * @return array(string)
+     */
     public static function all()
     {
         
     }
     
+    /**
+     * Checks if $type is a valid advice type.
+     *
+     * @param string $type
+     * @return boolean True if a valid type name is provided, false otherwise.
+     */
     public static function isValid($type)
     {
         
     }
     
+    /**
+     * Asserts tha $type is a valid advice type.
+     *
+     * Throws an exception if an invalid type is provided.
+     *
+     * @param string $type
+     * @throws InvalidArgumentException If an invalid type is provided.
+     */
     public static function assertValid($type)
     {
         
