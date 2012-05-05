24b71c555ffcd850a02ddd7353fc5291edf20a9d
Matthias Molitor <matthias@matthimatiker.de>
2012-05-05 22:17:03 +0200

    added test case stub

---
+++ tests/library/AspectPHP/Advice/TypeTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * AspectPHP_Advice_TypeTest
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 05.05.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the class that is responsible for type handling.
+ *
+ * @category PHP
+ * @package AspectPHP_Advice
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 05.05.2012
+ */
+class AspectPHP_Advice_TypeTest extends PHPUnit_Framework_TestCase
+{
+    
+    
+    
+}
