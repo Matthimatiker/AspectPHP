c3ea0ed051e6c36f77d5f41c834e555a4758f507
Matthias Molitor <matthias@matthimatiker.de>
2012-06-08 23:32:59 +0200

    added JoinPoint interface stub

---
+++ library/AspectPHP/IJoinPoint.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * AspectPHP_IJoinPoint
+ *
+ * @category PHP
+ * @package AspectPHP
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 08.06.2012
+ */
+
+/**
+ * Interface for classes that provide information about a join point.
+ *
+ * @category PHP
+ * @package AspectPHP
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 08.06.2012
+ */
+interface AspectPHP_IJoinPoint
+{
+    
+}
