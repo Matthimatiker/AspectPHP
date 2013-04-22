f7e12ffb33a1e1d835f182b5b0b6b9268ac85657
Matthias Molitor <matthias@matthimatiker.de>
2013-04-22 20:37:51 +0200

    replaced autoload function by Composer autoloader

--- Environment.config.php
+++ Environment.config.php
@@ -17,24 +17,4 @@ error_reporting(E_ALL | E_STRICT);
 
 set_include_path(dirname(__FILE__) . '/library' . PATH_SEPARATOR . get_include_path());
 
-/**
- * Tries to load the class or interface $class.
- *
- * Replaces undescores by slashes to create the file path.
- * Requires PHP 5.3 because stream_resolve_include_path is used.
- *
- * @param string $class
- * @return boolean False if the class was not loaded.
- */
-function autoload($class)
-{
-    $file = str_replace(array('_', '\\'), '/', $class) . '.php';
-    $path = stream_resolve_include_path($file);
-    if ($path === false) {
-        return false;
-    }
-    require($path);
-    return true;
-}
-
-spl_autoload_register('autoload');
+require_once(dirname(__FILE__) . '/vendor/autoload.php');
