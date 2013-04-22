e079c4d6b422739a17c302630736e24c5869ab37
Matthias Molitor <matthias@matthimatiker.de>
2013-04-22 21:01:07 +0200

    added test case stub

---
+++ tests/library/AspectPHP/EnvironmentTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * AspectPHP_EnvironmentTest
+ *
+ * @category PHP
+ * @package AspectPHP
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 22.04.2013
+ */
+
+/**
+ * Tests the environment intializer.
+ *
+ * @category PHP
+ * @package AspectPHP
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 22.04.2013
+ */
+class AspectPHP_EnvironmentTest extends PHPUnit_Framework_TestCase
+{
+    
+}
